b147fa3f7d259dfc2f9a0df9a838f42793ea9fe1
李华龙 <hualong.li@corp.elong.com>
2017-04-26 09:49:39 +0800

    confirm 入库

--- src/main/java/com/elong/hotel/hotelconfirm/confirmorder/context/ConfirmOrderDomainContext.java
+++ src/main/java/com/elong/hotel/hotelconfirm/confirmorder/context/ConfirmOrderDomainContext.java
@@ -3,16 +3,25 @@ package com.elong.hotel.hotelconfirm.confirmorder.context;
 import com.elong.hotel.hotelconfirm.confirmorder.enums.ServerRunningDomainEnum;
 
 import java.io.Serializable;
+import java.util.Map;
 
 public class ConfirmOrderDomainContext implements Serializable {
 
     private Integer reserNo;
+
+    private Map<String,Object> params;
 
     private ServerRunningDomainEnum serverRuningDomainEnum;
 
     public ConfirmOrderDomainContext(Integer reserNo, ServerRunningDomainEnum domainEnum) {
         this.reserNo = reserNo;
         this.serverRuningDomainEnum = domainEnum;
+    }
+
+    public ConfirmOrderDomainContext(Integer reserNo, ServerRunningDomainEnum domainEnum,Map<String,Object> params) {
+        this.reserNo = reserNo;
+        this.serverRuningDomainEnum = domainEnum;
+        this.params = params;
     }
 
     public Integer getReserNo() {
@@ -29,5 +38,13 @@ public class ConfirmOrderDomainContext implements Serializable {
 
     public void setServerRuningDomainEnum(ServerRunningDomainEnum serverRuningDomainEnum) {
         this.serverRuningDomainEnum = serverRuningDomainEnum;
+    }
+
+    public Map<String, Object> getParams() {
+        return params;
+    }
+
+    public void setParams(Map<String, Object> params) {
+        this.params = params;
     }
 }
