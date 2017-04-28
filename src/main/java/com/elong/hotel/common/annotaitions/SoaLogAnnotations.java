0e97552447f8bd6d10ae3e6e762e7e005d8023b8
boyu.yang <boyu.yang@corp.elong.com>
2017-04-28 17:18:19 +0800

    增加Mock  测试代码;

---
+++ src/main/java/com/elong/hotel/common/annotaitions/SoaLogAnnotations.java
@@ -0,0 +1,20 @@
+package com.elong.hotel.common.annotaitions;
+
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.*;
+
+
+//@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Component
+public @interface SoaLogAnnotations {
+
+    /**
+     * The value may indicate a suggestion for a logical component name,
+     * to be turned into a Spring bean in case of an autodetected component.
+     * @return the suggested component name, if any
+     */
+    String value() default "";
+}
