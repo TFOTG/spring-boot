637c98450d3b869a82e04f92493260901d8f9cd0
peng.wang <peng.wang@corp.elong.con>
2017-05-16 18:05:54 +0800

    log 更换位置

--- src/main/java/com/elong/hotel/log/hotelconfirm/bo/ConfirmServiceLogBo.java
+++ src/main/java/com/elong/hotel/log/hotelconfirm/bo/ConfirmServiceLogBo.java
@@ -25,6 +25,32 @@ public class ConfirmServiceLogBo {
 
     private Date opDate;
 
+    /**
+     * 查询开始时间
+     */
+    private String startTime;
+
+    /**
+     * 查询结束时间
+     */
+    private String endTime;
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
     public String getTableName() {
         return tableName;
     }
