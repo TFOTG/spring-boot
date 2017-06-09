7931815d77f68e8110240eb2869c56076f12ecf8
boyu.yang <boyu.yang@corp.elong.com>
2017-06-09 11:00:45 +0800

    增加实体数据.

---
+++ src/main/java/com/elong/hotel/hotelconfirm/confirmorder/request/CanUrgeRequest.java
@@ -0,0 +1,18 @@
+package com.elong.hotel.hotelconfirm.confirmorder.request;
+
+/**
+ * Created by peng.wang on 17/6/6.
+ */
+public class CanUrgeRequest {
+
+    private Integer orderId;
+
+    public Integer getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Integer orderId) {
+        this.orderId = orderId;
+    }
+
+}
