ec82aeb77587f81a3bd1dff46ee745015042851d
boyu.yang <boyu.yang@corp.elong.com>
2017-06-09 14:14:35 +0800

    调整代码逻辑,去掉关于已审getOrder 日志记录代码.

--- src/main/java/com/elong/hotel/hotelconfirm/confirmorder/response/ServerAction4ChangeOrderStatusResponse.java
+++ src/main/java/com/elong/hotel/hotelconfirm/confirmorder/response/ServerAction4ChangeOrderStatusResponse.java
@@ -6,11 +6,11 @@ import java.util.Date;
 /**
  * 执行command的结果
  */
-public class ServerAction4ConfirmResponse extends ServerActionResponseBase implements Serializable{
+public class ServerAction4ChangeOrderStatusResponse extends ServerActionResponseBase implements Serializable{
 
 	private Date promiseTime;
 
-	public ServerAction4ConfirmResponse() {
+	public ServerAction4ChangeOrderStatusResponse() {
 
 	}
 
