1db59062c4c6e60d7d4cf7a8b1ed58b1e41dfb0b
boyu.yang <boyu.yang@corp.elong.com>
2017-06-11 11:12:30 +0800

    调整代码逻辑,去掉关于已审getOrder 日志记录代码.

---
+++ src/main/java/com/elong/hotel/hotelconfirm/confirmorderactionlog/enums/ActionLogTypeEnum.java
@@ -0,0 +1,50 @@
+package com.elong.hotel.hotelconfirm.confirmorderactionlog.enums;
+
+/**
+ * Created by yangboyu on 17/4/6.
+ */
+public enum ActionLogTypeEnum {
+    /**
+     * Ebooking拒单
+     */
+    EbookingRefuse(1, "Ebooking拒单"),
+    /**
+     * 初拒
+     */
+    FirstRefuse(2, "初拒"),
+    /**
+     * 暂缓
+     */
+    Pause(3, "暂缓"),
+    /**
+     * 需担保/需同意
+     */
+    SpecialStatus(4, "需担保/需同意");
+
+    private int key;
+    private String value;
+
+    ActionLogTypeEnum(int key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public static String getValue(int key) {
+        String result = "";
+        for (ActionLogTypeEnum departmentEnum : ActionLogTypeEnum.values()) {
+            if (departmentEnum.getKey() == key) {
+                result = departmentEnum.getValue();
+                break;
+            }
+        }
+        return result;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
