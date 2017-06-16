71b10eeab59339ae3586dfad1246c239cdff0afe
boyu.yang <boyu.yang@corp.elong.com>
2017-06-16 09:54:02 +0800

    调整代码逻辑.

---
+++ src/main/java/com/elong/hotel/common/consts/MessageNameConst.java
@@ -0,0 +1,14 @@
+package com.elong.hotel.common.consts;
+
+/**
+ *
+ */
+public class MessageNameConst {
+
+    public static final String ORDER_VISUALIZATION="OrderVisualization";
+
+    public static final String c="Confirm";
+
+    public static final String REVIEW_VERIFY_REPEAT_LINKED="ReviewVerifyRepeatLinked";
+
+}
