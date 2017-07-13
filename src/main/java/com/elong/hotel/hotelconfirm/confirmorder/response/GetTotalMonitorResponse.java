5040a0a6f45bbfa4775d16338f2e1ee265ed9d2c
peng.wang <peng.wang@corp.elong.con>
2017-07-13 11:34:38 +0800

    修改兼容性样式,增加总库监控页面

--- src/main/java/com/elong/hotel/hotelconfirm/confirmorder/response/GetTotalMonitorResponse.java
+++ src/main/java/com/elong/hotel/hotelconfirm/confirmorder/response/GetTotalMonitorResponse.java
@@ -13,11 +13,22 @@ public class GetTotalMonitorResponse extends ServiceResponseBase {
 
     private List<TotalMonitorVO> list;
 
+    //最后一行总计的数据
+    private TotalMonitorVO total;
+
     public List<TotalMonitorVO> getList() {
         return list;
     }
 
     public void setList(List<TotalMonitorVO> list) {
         this.list = list;
+    }
+
+    public TotalMonitorVO getTotal() {
+        return total;
+    }
+
+    public void setTotal(TotalMonitorVO total) {
+        this.total = total;
     }
 }
