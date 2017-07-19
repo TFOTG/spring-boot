f758688db303b7493fe13262b637bb8870bf397a
张大鹏 <dapeng.zhang@corp.elong.com>
2017-07-20 07:38:17 +0800

    增加员工监控页面po,bo类文件

---
+++ src/main/java/com/elong/hotel/hotelconfirm/staff/po/StaffLogPo.java
@@ -0,0 +1,46 @@
+package com.elong.hotel.hotelconfirm.staff.po;
+
+import java.util.Date;
+
+public class StaffLogPo {
+
+	/**
+	 * 员工名称
+	 */
+	private String staffName;
+
+	/**
+	 * 登陆／登出时间
+	 */
+	private Date opdate;
+
+	/**
+	 * 登陆（0）／登出（1）
+	 */
+	private int logFlag;
+
+	public String getStaffName() {
+		return staffName;
+	}
+
+	public void setStaffName(String staffName) {
+		this.staffName = staffName;
+	}
+
+	public Date getOpdate() {
+		return opdate;
+	}
+
+	public void setOpdate(Date opdate) {
+		this.opdate = opdate;
+	}
+
+	public int getLogFlag() {
+		return logFlag;
+	}
+
+	public void setLogFlag(int logFlag) {
+		this.logFlag = logFlag;
+	}
+
+}
