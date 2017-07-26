018ec20da6d642fcaab7f51c8fed8e4388286a47
张大鹏 <dapeng.zhang@corp.elong.com>
2017-07-26 11:35:16 +0800

    修改显示时间段格式化

--- src/main/java/com/elong/hotel/hotelconfirm/staff/response/GetStaffMonitorResponse.java
+++ src/main/java/com/elong/hotel/hotelconfirm/staff/response/GetStaffMonitorResponse.java
@@ -1,11 +1,19 @@
 package com.elong.hotel.hotelconfirm.staff.response;
 
+import java.util.Date;
 import java.util.List;
 
+import com.alibaba.fastjson.annotation.JSONField;
 import com.elong.hotel.hotelconfirm.staff.bo.StaffMonitorBo;
 import com.elong.hotel.mis.common.model.response.ResponseBase;
 
 public class GetStaffMonitorResponse extends ResponseBase {
+
+	@JSONField(format = "hh:mm")
+	private Date lastBeginTime;
+
+	@JSONField(format = "hh:mm")
+	private Date lastEndTime;
 
 	private List<StaffMonitorBo> list;
 
@@ -15,6 +23,22 @@ public class GetStaffMonitorResponse extends ResponseBase {
 
 	public void setList(List<StaffMonitorBo> list) {
 		this.list = list;
+	}
+
+	public Date getLastBeginTime() {
+		return lastBeginTime;
+	}
+
+	public void setLastBeginTime(Date lastBeginTime) {
+		this.lastBeginTime = lastBeginTime;
+	}
+
+	public Date getLastEndTime() {
+		return lastEndTime;
+	}
+
+	public void setLastEndTime(Date lastEndTime) {
+		this.lastEndTime = lastEndTime;
 	}
 
 }
