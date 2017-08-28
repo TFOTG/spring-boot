0232e1981f0dc978c104786d702d72734500f0a1
peng.wang <peng.wang@corp.elong.con>
2017-08-28 17:33:34 +0800

    保存serviceLog压缩字段

--- src/main/java/com/elong/hotel/common/helper/CompressHelper.java
+++ src/main/java/com/elong/hotel/common/helper/CompressHelper.java
@@ -3,52 +3,92 @@ package com.elong.hotel.common.helper;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.util.zip.GZIPInputStream;
-import java.util.zip.GZIPOutputStream;
+import java.util.zip.*;
 
 /**
  * 文本压缩工具类
  */
 public class CompressHelper {
 
-	/**
-	 * 压缩
-	 * 
-	 * @param str
-	 * @return
-	 * @throws IOException
-	 */
-	public static String compress(String str) throws IOException {
-		if (str == null || str.length() == 0) {
-			return str;
-		}
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		GZIPOutputStream gzip = new GZIPOutputStream(out);
-		gzip.write(str.getBytes());
-		gzip.close();
-		return out.toString("ISO-8859-1");
-	}
 
-	/**
-	 * 解压缩
-	 * 
-	 * @param str
-	 * @return
-	 * @throws IOException
-	 */
-	public static String unCompress(String str) throws IOException {
-		if (str == null || str.length() == 0) {
-			return str;
-		}
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		ByteArrayInputStream in = new ByteArrayInputStream(str.getBytes("ISO-8859-1"));
-		GZIPInputStream gunzip = new GZIPInputStream(in);
-		byte[] buffer = new byte[256];
-		int n;
-		while ((n = gunzip.read(buffer)) >= 0) {
-			out.write(buffer, 0, n);
-		}
-		// toString()使用平台默认编码，也可以显式的指定如toString(&quot;GBK&quot;)
-		return out.toString();
-	}
+    /**
+     * 使用gzip进行压缩
+     */
+
+    public static String compressByGzip(String primStr) {
+        if (primStr == null || primStr.length() == 0) {
+            return primStr;
+        }
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        GZIPOutputStream gzipOutput = null;
+        try {
+            gzipOutput = new GZIPOutputStream(out);
+            gzipOutput.write(primStr.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (gzipOutput != null) {
+                try {
+                    gzipOutput.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return new sun.misc.BASE64Encoder().encode(out.toByteArray());
+    }
+
+    /**
+     * <p>Description:使用gzip进行解压缩</p>
+     *
+     * @param compressedStr
+     * @return
+     */
+    public static String unCompressByGzip(String compressedStr) {
+        if (compressedStr == null) {
+            return null;
+        }
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        ByteArrayInputStream in = null;
+        GZIPInputStream gzipInput = null;
+        byte[] compressed;
+        String decompressed = null;
+        try {
+            compressed = new sun.misc.BASE64Decoder().decodeBuffer(compressedStr);
+            in = new ByteArrayInputStream(compressed);
+            gzipInput = new GZIPInputStream(in);
+
+            byte[] buffer = new byte[1024];
+            int offset;
+            while ((offset = gzipInput.read(buffer)) != -1) {
+                out.write(buffer, 0, offset);
+            }
+            decompressed = out.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (gzipInput != null) {
+                try {
+                    gzipInput.close();
+                } catch (IOException e) {
+                }
+            }
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                }
+            }
+            if (out != null) {
+                try {
+                    out.close();
+                } catch (IOException e) {
+                }
+            }
+        }
+
+        return decompressed;
+    }
+
 }
