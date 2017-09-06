cbad4e474213755b22fef0162d180f9ff3abfbf7
张大鹏 <dapeng.zhang@corp.elong.com>
2017-09-06 17:21:13 +0800

    增加取描述字段

--- src/main/java/com/elong/hotel/hotelconfirm/noroomorder/enums/NoRoomIVRStatusEnum.java
+++ src/main/java/com/elong/hotel/hotelconfirm/noroomorder/enums/NoRoomIVRStatusEnum.java
@@ -5,37 +5,47 @@ package com.elong.hotel.hotelconfirm.noroomorder.enums;
  */
 public enum NoRoomIVRStatusEnum {
 
-    NEW(0,"新建"),
+	NEW(0, "新建"),
 
-    PROCESS(1,"处理中"),
+	PROCESS(1, "处理中"),
 
-    SUCCEED_FULL(2,"成功反馈满房"),
+	SUCCEED_FULL(2, "成功反馈满房"),
 
-    SUCCEED_NOFUll(3,"成功反馈未满房"),
+	SUCCEED_NOFUll(3, "成功反馈未满房"),
 
-    FAIL_TIMEOUT(4,"失败超时"),
+	FAIL_TIMEOUT(4, "失败超时"),
 
-    FAIL_NO_FEEDBACK(5,"失败无反馈"),
+	FAIL_NO_FEEDBACK(5, "失败无反馈"),
 
-    FAIL_NO_IVR(6,"失败并且不外呼"),
+	FAIL_NO_IVR(6, "失败并且不外呼"),
 
-    FAIL_VALIDATION(7,"校验失败");
+	FAIL_VALIDATION(7, "校验失败");
 
-    private int status;
+	private int status;
 
-    private String desc;
+	private String desc;
 
+	public int getStatus() {
+		return status;
+	}
 
-    public int getStatus() {
-        return status;
-    }
+	public String getDesc() {
+		return desc;
+	}
 
-    public String getDesc() {
-        return desc;
-    }
+	NoRoomIVRStatusEnum(int status, String desc) {
+		this.status = status;
+		this.desc = desc;
+	}
 
-    NoRoomIVRStatusEnum(int status, String desc) {
-        this.status = status;
-        this.desc = desc;
-    }
+	public static String getDescByStatus(int key) {
+		String result = "";
+		for (NoRoomIVRStatusEnum noRoomIVRStatusEnum : NoRoomIVRStatusEnum.values()) {
+			if (noRoomIVRStatusEnum.getStatus() == key) {
+				result = noRoomIVRStatusEnum.getDesc();
+				break;
+			}
+		}
+		return result;
+	}
 }
--- src/main/java/com/elong/hotel/hotelconfirm/noroomorder/enums/NoRoomStaffStatusEnum.java
+++ src/main/java/com/elong/hotel/hotelconfirm/noroomorder/enums/NoRoomStaffStatusEnum.java
@@ -5,27 +5,40 @@ package com.elong.hotel.hotelconfirm.noroomorder.enums;
  */
 public enum NoRoomStaffStatusEnum {
 
-    NoDeal(0,"员工未处理"),
+	NoDeal(0, "员工未处理"),
 
-    Deal(1,"员工已处理");
+	OriginalRoomArrangement(1, "原房安排"),
 
+	FreeUpgrade(2, "免费升级"),
 
+	DealFailed(3, "干预失败");
 
-    private int status;
+	private int status;
 
-    private String desc;
+	private String desc;
 
-    public int getStatus() {
-        return status;
-    }
+	public int getStatus() {
+		return status;
+	}
 
-    public String getDesc() {
-        return desc;
-    }
+	public String getDesc() {
+		return desc;
+	}
 
-    NoRoomStaffStatusEnum(int status, String desc) {
-        this.status = status;
-        this.desc = desc;
-    }
+	NoRoomStaffStatusEnum(int status, String desc) {
+		this.status = status;
+		this.desc = desc;
+	}
+
+	public static String getDescByStatus(int key) {
+		String result = "";
+		for (NoRoomStaffStatusEnum noRoomStaffStatusEnum : NoRoomStaffStatusEnum.values()) {
+			if (noRoomStaffStatusEnum.getStatus() == key) {
+				result = noRoomStaffStatusEnum.getDesc();
+				break;
+			}
+		}
+		return result;
+	}
 
 }
