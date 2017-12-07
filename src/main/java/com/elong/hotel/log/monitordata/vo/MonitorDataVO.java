6a4d9f4a20c11f2e5692f9b4cadc39bfe11a50e6
张大鹏 <dapeng.zhang@corp.elong.com>
2017-12-07 16:49:36 +0800

    删除无效的引用

--- src/main/java/com/elong/hotel/log/monitordata/vo/MonitorDataVO.java
+++ src/main/java/com/elong/hotel/log/monitordata/vo/MonitorDataVO.java
@@ -1,55 +1,53 @@
 package com.elong.hotel.log.monitordata.vo;
 
-import com.elong.hotel.common.parameter.PaginationParameter;
-import com.sun.tools.corba.se.idl.StringGen;
-
 import java.util.Date;
+
+import com.elong.hotel.common.parameter.PaginationParameter;
 
 /**
  * @outhor jianjun.wang
  * @create 2017-12-05 下午4:50
  */
 
-public class MonitorDataVO extends PaginationParameter{
+public class MonitorDataVO extends PaginationParameter {
 
+	/**
+	 * 获取数据类型
+	 */
+	private Integer dataType;
 
-    /**
-     * 获取数据类型
-     */
-    private Integer dataType;
+	/**
+	 *时间轴参数 start
+	 */
+	private Date startCollectTime;
 
-    /**
-     *时间轴参数 start
-     */
-    private Date startCollectTime;
+	/**
+	 * 时间轴参数 end
+	 */
+	private Date endCollectTime;
 
-    /**
-     * 时间轴参数 end
-     */
-    private Date endCollectTime;
+	public Date getStartCollectTime() {
+		return startCollectTime;
+	}
 
-    public Date getStartCollectTime() {
-        return startCollectTime;
-    }
+	public void setStartCollectTime(Date startCollectTime) {
+		this.startCollectTime = startCollectTime;
+	}
 
-    public void setStartCollectTime(Date startCollectTime) {
-        this.startCollectTime = startCollectTime;
-    }
+	public Date getEndCollectTime() {
+		return endCollectTime;
+	}
 
-    public Date getEndCollectTime() {
-        return endCollectTime;
-    }
+	public void setEndCollectTime(Date endCollectTime) {
+		this.endCollectTime = endCollectTime;
+	}
 
-    public void setEndCollectTime(Date endCollectTime) {
-        this.endCollectTime = endCollectTime;
-    }
+	public Integer getDataType() {
+		return dataType;
+	}
 
-    public Integer getDataType() {
-        return dataType;
-    }
-
-    public void setDataType(Integer dataType) {
-        this.dataType = dataType;
-    }
+	public void setDataType(Integer dataType) {
+		this.dataType = dataType;
+	}
 
 }
