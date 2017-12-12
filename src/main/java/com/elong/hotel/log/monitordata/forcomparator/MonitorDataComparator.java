0997946962f7a2e04e12dac7a50abcbcfc14eec4
jianjun.wang <jianjun.wang@corp.elong.com>
2017-12-12 10:44:02 +0800

    数据查询排序类

---
+++ src/main/java/com/elong/hotel/log/monitordata/forcomparator/MonitorDataComparator.java
@@ -0,0 +1,22 @@
+package com.elong.hotel.log.monitordata.forcomparator;
+
+import com.elong.hotel.log.monitordata.bo.MonitorDataBo;
+
+import java.util.Comparator;
+
+/**
+ * @outhor jianjun.wang
+ * @create 2017-12-11 下午3:04
+ */
+
+public class MonitorDataComparator implements Comparator<MonitorDataBo> {
+
+    @Override
+    public int compare(MonitorDataBo o1, MonitorDataBo o2) {
+        if (o1.getCollectTime().compareTo(o2.getCollectTime()) < 0) {
+            return 1;
+        }else {
+            return 1;
+        }
+    }
+}
