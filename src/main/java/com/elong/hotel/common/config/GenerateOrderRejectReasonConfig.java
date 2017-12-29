ac897683234fa498f16b33b4db3fa4e08232f874
jianjun.wang <jianjun.wang@corp.elong.com>
2017-12-29 16:10:16 +0800

    feat:特满小状态变更,新增o7,o8两个小状态 ，隐藏前台展示O1、G2状态,G1展示变为 价格未出

--- src/main/java/com/elong/hotel/common/config/GenerateOrderRejectReasonConfig.java
+++ src/main/java/com/elong/hotel/common/config/GenerateOrderRejectReasonConfig.java
@@ -1,5 +1,6 @@
 package com.elong.hotel.common.config;
 
+import com.alibaba.fastjson.JSON;
 import com.elong.common.util.JsonUtil;
 
 import java.util.ArrayList;
@@ -23,31 +24,52 @@ public class GenerateOrderRejectReasonConfig {
         O1.put("sendsms", "N");
         O1.put("detail", "房间满");
         O1.put("id", "1");
+        O1.put("show", "0");
         Map<String, String> O2 = new HashMap<String, String>();
         O2.put("ostatus", "O2");
         O2.put("sendsms", "N");
         O2.put("detail", "联系不上");
         O2.put("id", "2");
+        O2.put("show", "1");
         Map<String, String> O3 = new HashMap<String, String>();
         O3.put("ostatus", "O3");
         O3.put("sendsms", "N");
         O3.put("detail", "号码无效");
         O3.put("id", "3");
+        O3.put("show", "1");
         Map<String, String> O4 = new HashMap<String, String>();
         O4.put("ostatus", "O4");
         O4.put("sendsms", "N");
         O4.put("detail", "不接线下");
         O4.put("id", "4");
+        O4.put("show", "1");
         Map<String, String> O5 = new HashMap<String, String>();
         O5.put("ostatus", "O5");
         O5.put("sendsms", "N");
         O5.put("detail", "不合作");
         O5.put("id", "5");
+        O5.put("show", "1");
         Map<String, String> O6 = new HashMap<String, String>();
         O6.put("ostatus", "O6");
         O6.put("sendsms", "N");
         O6.put("detail", "eb满房");
         O6.put("id", "6");
+        O6.put("show", "1");
+        Map<String, String> O7 = new HashMap<String, String>();
+        O7.put("ostatus", "O7");
+        O7.put("sendsms", "N");
+        O7.put("detail", "全满");
+        O7.put("id", "19");
+        O7.put("show", "1");
+        Map<String, String> O8 = new HashMap<String, String>();
+        O8.put("ostatus", "O8");
+        O8.put("sendsms", "N");
+        O8.put("detail", "部分满");
+        O8.put("id", "20");
+        O8.put("show", "1");
+
+        fullRoom.add(O7);
+        fullRoom.add(O8);
         fullRoom.add(O1);
         fullRoom.add(O2);
         fullRoom.add(O3);
@@ -60,18 +82,21 @@ public class GenerateOrderRejectReasonConfig {
         Map<String, String> G1 = new HashMap<String, String>();
         G1.put("ostatus", "G1");
         G1.put("sendsms", "N");
-        G1.put("detail", "变价");
+        G1.put("detail", "价格已出");
         G1.put("id", "7");
+        G1.put("show", "1");
         Map<String, String> G2 = new HashMap<String, String>();
         G2.put("ostatus", "G2");
         G2.put("sendsms", "N");
         G2.put("detail", "暂不接受预订");
         G2.put("id", "8");
+        G2.put("show", "0");
         Map<String, String> G3 = new HashMap<String, String>();
         G3.put("ostatus", "G3");
         G3.put("sendsms", "N");
         G3.put("detail", "价格未出");
         G3.put("id", "9");
+        G3.put("show", "1");
         changePrice.add(G1);
         changePrice.add(G2);
         changePrice.add(G3);
@@ -86,11 +111,13 @@ public class GenerateOrderRejectReasonConfig {
         U1.put("detail", "需担保");
         U1.put("id", "10");
         U1.put("messageForview", "酒店临时通知需要信用卡担保,同意,请等待客服30分钟联系您处理;取消此单,请点击“取消订单”");
+        U1.put("show", "1");
         Map<String, String> U2 = new HashMap<String, String>();
         U2.put("ostatus", "U2");
         U2.put("sendsms", "N");
         U2.put("detail", "其他");
         U2.put("id", "11");
+        U2.put("show", "0");
         needGuarantee.add(U1);
         needGuarantee.add(U2);
 
@@ -104,6 +131,7 @@ public class GenerateOrderRejectReasonConfig {
         U3.put("detail", "重复预订");
         U3.put("id", "12");
         U3.put("messageForview", "酒店临时通知您有重复预订,无重复预订,请即点击“同意安排”;取消此单,请点击“取消订单”");
+        U3.put("show", "1");
         Map<String, String> U4 = new HashMap<String, String>();
         U4.put("ostatus", "U4");
         U4.put("messageToguest", "[艺龙]订单***********酒店通知***回复BG**,取消此单回复BY**,或登录app-我的-我的订单操作");
@@ -112,6 +140,7 @@ public class GenerateOrderRejectReasonConfig {
         U4.put("detail", "客人订错***");
         U4.put("id", "13");
         U4.put("messageForview", "酒店临时通知***,请即点击“同意安排”;取消此单,请点击“取消订单”");
+        U4.put("show", "1");
         Map<String, String> U5 = new HashMap<String, String>();
         U5.put("ostatus", "U5");
         U5.put("messageToguest", "[艺龙]订单***********酒店告知您曾致电酒店取消此单,保留此单回复BG**,取消此订单回复BY**,或登录app-我的-我的订单操作");
@@ -120,6 +149,7 @@ public class GenerateOrderRejectReasonConfig {
         U5.put("detail", "核实客人取消");
         U5.put("id", "14");
         U5.put("messageForview", "酒店临时通知您有订单需要取消,保留订单,请即点击“同意安排”;取消此单,请点击“取消订单”");
+        U5.put("show", "1");
         Map<String, String> U6 = new HashMap<String, String>();
         U6.put("ostatus", "U6");
         U6.put("messageToguest", "[艺龙]订单***********酒店通知***回复BG**,取消此单回复BY**,如有疑问请致电4009333333-1");
@@ -128,6 +158,7 @@ public class GenerateOrderRejectReasonConfig {
         U6.put("detail", "因****可能会影响入住");
         U6.put("id", "15");
         U6.put("messageForview", "酒店临时通知***,同意,请即点击“同意安排”;取消此单,请点击“取消订单”");
+        U6.put("show", "1");
         Map<String, String> U7 = new HashMap<String, String>();
         U7.put("ostatus", "U7");
         U7.put("messageToguest", "[艺龙]订单***********酒店正在为您协调房间,同意等待回复BG**,取消此单回复BY**,或登录app-我的-我的订单操作");
@@ -136,6 +167,7 @@ public class GenerateOrderRejectReasonConfig {
         U7.put("detail", "马上到店不处理");
         U7.put("id", "16");
         U7.put("messageForview", "酒店临时通知正在为您协调房间,同意等待,请即点击“同意安排”;取消此单,请点击“取消订单”");
+        U7.put("show", "1");
         Map<String, String> U8 = new HashMap<String, String>();
         U8.put("ostatus", "U8");
         U8.put("messageToguest", "[艺龙]订单***********酒店通知***回复BG**,取消此单回复BY**,如有疑问请致电4009333333-1");
@@ -144,11 +176,13 @@ public class GenerateOrderRejectReasonConfig {
         U8.put("detail", "可为您免费升级到****");
         U8.put("id", "17");
         U8.put("messageForview", "酒店临时通知***,请即点击“同意安排”;取消此单,请点击“取消订单”");
+        U8.put("show", "1");
         Map<String, String> U9 = new HashMap<String, String>();
         U9.put("ostatus", "U9");
         U9.put("sendsms", "N");
         U9.put("detail", "其他");
         U9.put("id", "18");
+        U9.put("show", "1");
         specialFullroom.add(U3);
         specialFullroom.add(U4);
         specialFullroom.add(U5);
@@ -187,6 +221,8 @@ public class GenerateOrderRejectReasonConfig {
         mapMeum.put("specialFullroom", specialFullroom);
         mapMeum.put("menuonenames", menuonenames);
 
-        System.out.println(JsonUtil.BeanToJson(mapMeum));
+        System.out.println(JSON.toJSONString(mapMeum));
+
+//        System.out.println(JsonUtil.BeanToJson(mapMeum));
     }
 }
