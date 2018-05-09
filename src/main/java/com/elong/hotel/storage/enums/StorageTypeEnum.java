b9366860e4f564c01afcc12eaef6a879de74fc16
peng.wang <peng.wang@corp.elong.con>
2018-05-09 16:37:54 +0800

    feat:终拒如果bug fix

--- src/main/java/com/elong/hotel/storage/enums/StorageTypeEnum.java
+++ src/main/java/com/elong/hotel/storage/enums/StorageTypeEnum.java
@@ -1,5 +1,8 @@
 package com.elong.hotel.storage.enums;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Hualong.Li on 17/4/24.
  */
@@ -22,4 +25,12 @@ public enum StorageTypeEnum {
 	public int getValue() {
 		return value;
 	}
+
+	public static List<StorageTypeEnum> getEnumList(){
+        List<StorageTypeEnum> list =new ArrayList<>();
+        for (StorageTypeEnum storageTypeEnum : StorageTypeEnum.values()) {
+            list.add(storageTypeEnum);
+        }
+        return list;
+    }
 }
