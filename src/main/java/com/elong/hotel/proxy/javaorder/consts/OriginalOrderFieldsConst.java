d640a25267bcdc012a85ad14ce833ba90db9f943
张大鹏 <dapeng.zhang@corp.elong.com>
2018-07-30 07:40:05 +0800

    feat:修改同程标识BUG

--- src/main/java/com/elong/hotel/proxy/javaorder/consts/OriginalOrderFieldsConst.java
+++ src/main/java/com/elong/hotel/proxy/javaorder/consts/OriginalOrderFieldsConst.java
@@ -68,7 +68,7 @@ public interface OriginalOrderFieldsConst {
 	/**
 	 * 终拒/员工待处理查询
 	 */
-	String SEARCH_FOR_EXAM = "status,hotelId,payment,payAmount,otaType,isUrgentVerify,additionalStatus,guestName,proxy,hotelName,gradeId,sumPrice,sumCost,contactPhone,contactMobile,orderFrom,ratePlanId,notesToHotel,notesToGuest,notesToElong,businessSystemId,roomCount,roomNightsCount,roomTypeId,roomTypeName,roomNightStayDate,roomNightIsSpecial,roomNightCost,roomNightSalePrice,roomNightRealSalePrice,roomNightRealCost,roomNightBreakfastCount,exchangedSumPrice,provinceId,orderKeyValues,additionalStatusI,promotionSettlementType,cityName,promotionChannelCode,tcOrderId,tcSubOrderId,tcCardNo,baiduLatitude,baiduLongitude,cooperationOrderId,baseRuleInfo,roomNightCurrencyCode,roomNightInventoryType";
+	String SEARCH_FOR_EXAM = "status,hotelId,payment,payAmount,otaType,isUrgentVerify,additionalStatus,guestName,proxy,hotelName,gradeId,sumPrice,sumCost,contactPhone,contactMobile,orderFrom,ratePlanId,notesToHotel,notesToGuest,notesToElong,businessSystemId,roomCount,roomNightsCount,roomTypeId,roomTypeName,roomNightStayDate,roomNightIsSpecial,roomNightCost,roomNightSalePrice,roomNightRealSalePrice,roomNightRealCost,roomNightBreakfastCount,exchangedSumPrice,provinceId,orderKeyValues,additionalStatusI,promotionSettlementType,cityName,promotionChannelCode,tcOrderId,tcSubOrderId,tcCardNo,baiduLatitude,baiduLongitude,cooperationOrderId,baseRuleInfo,roomNightCurrencyCode,roomNightInventoryType,originFrom";
 	
 	/**
 	 * 终拒查询备注信息
